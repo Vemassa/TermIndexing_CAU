d2d42e1d99fb4344c06cd834c0cdbfc9b9dd6201
Quentin Santos <quentin.santos@epitech.eu>
2020-04-06 14:06:34 +0200

    feat(posting): create posting list

--- main.py
+++ main.py
@@ -17,6 +17,23 @@ def retrieve_file_content(filename):
 def sort_indexes(list):
     return sorted(list, key=str.lower)
 
+def posting_list(list):
+    posting_list = []
+    last_elem = ""
+    i = 0
+
+    while i < len(list):
+        if i < 1:
+            posting_list.append([list[i][0], 1, [list[i][1]]])
+        elif list[i][0] != list[i - 1][0]:
+            posting_list.append([list[i][0], 1, [list[i][1]]])
+        elif list[i][1] not in posting_list[-1][2]:
+            posting_list[-1][1] += 1
+            posting_list[-1][2].append(list[i][1])
+        i += 1
+
+    # print(*posting_list, sep="\n")
+
 def main():
     print("Hello, World!")
 
@@ -30,7 +47,9 @@ def main():
             inverted_indexes += list([word, index] for word in words)
 
     inverted_indexes = sorted(inverted_indexes, key=lambda word: (word[0], word[1]))
-    print(*inverted_indexes, sep="\n")
+    posting_list(inverted_indexes)
+
+    # print(*inverted_indexes, sep="\n")
 
 if __name__== "__main__" :
     main()
